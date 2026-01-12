bd58b372fb17854d13d9da04ca13587722903426
Riz Josel Urian <rurian@apple.com>
2026-01-12 20:33:57 +0800

    add s3 in for loop

--- my_ec2_cdk_py/my_ec2_cdk_py_stack.py
+++ my_ec2_cdk_py/my_ec2_cdk_py_stack.py
@@ -20,7 +20,7 @@ class MyEc2CdkPyStack(Stack):
         for i in name:
             s3.Bucket(
                 self,
-                "f{i}",
+                f"{i}",
                 bucket_name=f"{i}using-python-cdk"
             )
 
