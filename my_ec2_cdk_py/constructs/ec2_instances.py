3b1ad936bcba2c82b5fe781d9b11048ffae4d360
Riz Josel Urian <rurian@apple.com>
2026-01-13 20:40:07 +0800

    fix_ec2_constructs

--- my_ec2_cdk_py/constructs/ec2_instances.py
+++ my_ec2_cdk_py/constructs/ec2_instances.py
@@ -1,4 +1,4 @@
-from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_ec2 as ec2, CfnOutput
 from constructs import Construct
 
 class Ec2InstanceConstruct(Construct):
@@ -9,25 +9,37 @@ class Ec2InstanceConstruct(Construct):
         *,
         vpc: ec2.Vpc,
         security_group: ec2.SecurityGroup,
-        name: str,
-        public: bool,
-        instance_type: str = "t2.micro",
-        key_name: str | None = None
+        instance_config: dict
     ):
         super().__init__(scope, id)
 
-        self.instance = ec2.Instance(
+        name = instance_config["name"]
+        public = instance_config["public"]
+
+        vpc_subnets = (
+            ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC)
+            if public else None
+        )
+
+        sg = security_group if public else None
+        key_name = "cli-user" if public else None
+
+        instance = ec2.Instance(
             self,
-            "Instance",
-            instance_type=ec2.InstanceType(instance_type),
+            f"{name}-instance",
+            vpc=vpc,
+            instance_type=ec2.InstanceType("t2.micro"),
             machine_image=ec2.AmazonLinuxImage(
                 generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
             ),
-            vpc=vpc,
-            vpc_subnets=ec2.SubnetSelection(
-                subnet_type=ec2.SubnetType.PUBLIC
-                if public else ec2.SubnetType.PRIVATE_WITH_EGRESS
-            ),
-            security_group=security_group,
-            key_name="cli-user" if public else None
+            vpc_subnets=vpc_subnets,
+            security_group=sg,
+            key_name=key_name
         )
+
+        if public:
+            CfnOutput(
+                self,
+                f"{name}_PublicIp",
+                value=instance.instance_public_ip
+            )
